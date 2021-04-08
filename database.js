e9fc8dca9b44f341a2564515e91bb08dfcc96daa
Damien Bone <dbone@liveperson.com>
2021-04-08 13:20:30 +0300

    Initial commit

---
+++ database.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+// Database Connection for Production
+
+let config = {
+user: "svc_lpsync",
+    database: "lpsync_db",
+    password: "LP@Sync2020!"
+ }
+
+ //if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+  config.socketPath = `/cloudsql/lpsync:us-central1:lpsync-db`;
+// }
+
+ let connection = mysql.createConnection(config);
+
+// Database Connection for Development
+
+// let connection = mysql.createConnection({
+//     host: "35.238.133.62",
+//     user: "svc_lpsync",
+//     database: "lpsync_db",
+//     password: "LP@Sync2020!"
+// });
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as thread id: ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+const connection = require('./../database');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Demo Site' });
+});
+
+
+
+router.route('/testsql')
+    .get(function(req, res, next) {
+
+        connection.query("SELECT * FROM LpsyncPermission", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+
+    });
+
+
+module.exports = router;
